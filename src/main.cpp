2ee9ed8b80e8d278338717da8f85a98a7041b3c9
mTvare6 <mTvare6@users.noreply.github.com>
2021-09-08 19:11:43 +0530

    add: size character

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,43 @@
 #include <SFML/Config.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
 #include <random>
 #include <string>
 #include <chrono>
+#include <variant>
+
 using namespace std::string_literals;
+sf::Font font1;
 
-
+// settings which change a lot of stuff
 constexpr float WIDTH {1400};
 constexpr float HEIGHT {900};
 constexpr float cutoff = 14;
 #define CROSS_RATE 1000000000
+std::uniform_int_distribution<> motion_dis(-6, 6);
+std::uniform_int_distribution<> small_dis(5, 20);
+std::uniform_int_distribution<> big_dist(30, 50);
 
-std::random_device rd; // obtain a random number from hardware
-std::mt19937 gen(rd()); // seed the generator
-std::uniform_int_distribution<> motion_dis(-4, 4);
-std::uniform_int_distribution<> binary_dist(0, 1);
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> binary_dis(0, 1);
+
+
 
 enum Color{
-  Green, // green is dominant
+  Green, // dominant
   Yellow,
+};
+enum Size{
+  Big,
+  Small,
 };
 
 typedef std::pair<Color, Color> ColorGenotype;
+typedef std::pair<Size , Size > SizeGenotype;
 
 sf::Color hex2rgb(const int hex){
   return sf::Color{static_cast<sf::Uint8>(((hex >> 16) & 0xFF)), static_cast<sf::Uint8>(((hex >> 8) & 0xFF)), static_cast<sf::Uint8>((hex & 0xFF))};
@@ -32,7 +46,7 @@ sf::Color hex2rgb(const int hex){
 class Blob
 {
   public:
-    Blob(sf::Vector2f=sf::Vector2f{0, 0}, ColorGenotype c={ColorGenotype{Green, Green}}, float size=25);
+    Blob(sf::Vector2f=sf::Vector2f{0, 0}, ColorGenotype c=ColorGenotype{Green, Green}, SizeGenotype sz=SizeGenotype{Big, Big});
     Blob(Blob &&) = default;
     Blob(const Blob &) = default;
     Blob(Blob *parent1, Blob *parent2);
@@ -41,14 +55,18 @@ class Blob
     ~Blob() = default;
 
     ColorGenotype c;
+    SizeGenotype sz;
     sf::Vector2f vector;
     float size;
     sf::CircleShape object;
+
+    sf::Text coltext;
+    sf::Text sizetext;
+
     std::chrono::steady_clock::time_point previous_cross;
 
     void update(int x, int y);
     bool isTouching(Blob *another);
-    void debug(int vectorIndex);
 
   private:
 
@@ -70,14 +88,39 @@ bool Blob::isTouching(Blob *another){
   auto y = std::minmax(this->vector.y, another->vector.y);
   auto x = std::minmax(this->vector.x, another->vector.x);
   float dist = ((y.second-y.first)*(y.second-y.first)+(x.second-x.first)*(x.second-x.first));
-  return dist <= (this->size+cutoff)*(this->size+cutoff);
+  return dist <= (this->size+another->size+cutoff)*(this->size+another->size+cutoff);
   /* float dist = std::sqrtf((y.second-y.first)*(y.second-y.first)+(x.second-x.first)*(x.second-x.first)); */
-  /* return dist <= (this->size+cutoff); */
+  /* return dist <= (this->size+another->size+cutoff); */
 }
 
-Blob::Blob(sf::Vector2f v, ColorGenotype c, float size){
+Blob::Blob(sf::Vector2f v, ColorGenotype c, SizeGenotype sz){
+
   this->c=c;
-  this->size = size;
+  this->sz=sz;
+
+  if(sz.first==Small || sz.second==Small) this->size = small_dis(gen);
+  else                                    this->size = big_dist(gen);
+  
+
+  std::string buf;
+  if(this->c.first==Green)  buf+="G";
+  else                      buf+="g";
+  if(this->c.second==Green) buf+="G";
+  else                      buf+="g";
+
+  this->coltext=sf::Text(buf, font1, this->size);
+  buf.clear();
+
+  if(this->sz.first==Big)    buf+="S";
+  else                       buf+="s";
+  if(this->sz.second==Small) buf+="S";
+  else                       buf+="s";
+
+  this->sizetext=sf::Text(buf, font1, this->size);
+  buf.clear();
+
+
+
   this->previous_cross = std::chrono::steady_clock::now();
   this->vector=v;
   this->object.setPosition(v);
@@ -96,13 +139,43 @@ Blob::Blob(sf::Vector2f v, ColorGenotype c, float size){
 
 Blob::Blob(Blob *parent1, Blob *parent2){
 
-  Color c1 = binary_dist(gen)==0?parent1->c.first:parent1->c.second;
-  Color c2 = binary_dist(gen)==0?parent2->c.first:parent2->c.second;
-  bool order = binary_dist(gen)==0;
+  bool order = binary_dis(gen)==0;
+
+  Color c1 = binary_dis(gen)==0?parent1->c.first:parent1->c.second;
+  Color c2 = binary_dis(gen)==0?parent2->c.first:parent2->c.second;
   this->c=ColorGenotype{
     order?c1:c2, order?c2:c1
   };
-  this->size = (parent1->size+parent2->size)/2;
+
+  Size sz1 = binary_dis(gen)==0?parent1->sz.first:parent1->sz.second;
+  Size sz2 = binary_dis(gen)==0?parent2->sz.first:parent2->sz.second;
+  this->sz=SizeGenotype{
+    order?sz1:sz2, order?sz2:sz1
+  };
+
+  if(this->sz.first==Small || this->sz.second==Small) this->size = small_dis(gen);
+  else                                                this->size = big_dist(gen);
+
+
+  std::string buf;
+  if(this->c.first==Green)  buf+="G";
+  else                      buf+="g";
+  if(this->c.second==Green) buf+="G";
+  else                      buf+="g";
+
+  this->coltext=sf::Text(buf, font1, this->size);
+  buf.clear();
+
+  if(this->sz.first==Big)    buf+="S";
+  else                       buf+="s";
+  if(this->sz.second==Small) buf+="S";
+  else                       buf+="s";
+
+  this->sizetext=sf::Text(buf, font1, this->size);
+  buf.clear();
+
+
+
   this->previous_cross = std::chrono::steady_clock::now();
   this->vector = parent1->vector;
   this->object = sf::CircleShape{this->size};
@@ -118,16 +191,6 @@ Blob::Blob(Blob *parent1, Blob *parent2){
     this->object.setOutlineColor(sf::Color{0xd79921ff});
   }
 }
-void Blob::debug(int vectorIndex){
-  std::string genome;
-  if(this->c.first==Green)  genome+="G";
-  else                      genome+="g";
-  if(this->c.second==Green) genome+="G";
-  else                      genome+="g";
-
-  std::cout <<  "Vector" << vectorIndex << ": X: " << this->vector.x << "  Y: " << this->vector.y << "  Genome: " << genome << std::endl;
-}
-
 
 
 
@@ -137,10 +200,11 @@ void Blob::debug(int vectorIndex){
 
 int main(int argc, char **argv) {
   sf::RenderWindow w(sf::VideoMode(WIDTH, HEIGHT), "", sf::Style::Resize);
+  font1.loadFromFile("scientifica Nerd Font.ttf");
 
   std::vector<Blob> blobs;
-  blobs.push_back(Blob{sf::Vector2f{(WIDTH/2), (HEIGHT/2)-(HEIGHT/4)}, ColorGenotype{Green, Yellow},  20});
-  blobs.push_back(Blob{sf::Vector2f{(WIDTH/2), (HEIGHT/2)+(HEIGHT/4)}, ColorGenotype{Green, Yellow},  20});
+  blobs.push_back(Blob{sf::Vector2f{(WIDTH/2), (HEIGHT/2)-(HEIGHT/4)}, ColorGenotype{Green , Green }, SizeGenotype{Big  , Big  }});
+  blobs.push_back(Blob{sf::Vector2f{(WIDTH/2), (HEIGHT/2)+(HEIGHT/4)}, ColorGenotype{Yellow, Yellow}, SizeGenotype{Small, Small}});
 
   while (w.isOpen()) {
     sf::Event event;
@@ -176,7 +240,11 @@ int main(int argc, char **argv) {
         }
       }
       blobs[i].update(blobs[i].vector.x+motion_dis(gen), blobs[i].vector.y+motion_dis(gen)  );
+      blobs[i].coltext.setPosition(blobs[i].vector);
+      blobs[i].sizetext.setPosition(blobs[i].vector.x, blobs[i].vector.y+blobs[i].size);
       w.draw(blobs[i].object);
+      w.draw(blobs[i].coltext);
+      w.draw(blobs[i].sizetext);
     }
     w.display();
   }
